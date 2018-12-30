248b02d30710f43a84b318fef5bed51acc3579f0
wosad878 <wosad878@naver.com>
2018-12-30 15:08:57 +0900

    로그인 후처리

--- app/src/main/java/com/iu/serverapitest/MainActivity.java
+++ app/src/main/java/com/iu/serverapitest/MainActivity.java
@@ -1,14 +1,20 @@
 package com.iu.serverapitest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.TextView userNameTxt;
+    private android.widget.TextView userIDTxt;
+    private android.widget.TextView userEmailTxt;
+    private android.widget.TextView userPhoneTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         bindViews();
         setupEvents();
@@ -22,11 +28,24 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        String loginId = getIntent().getStringExtra("로그인아이디");
+        String loginEmail = getIntent().getStringExtra("이메일");
+        String loginName = getIntent().getStringExtra("이름");
+        String loginPhone = getIntent().getStringExtra("폰번");
+
+        userNameTxt.setText(loginName);
+        userIDTxt.setText(loginId);
+        userEmailTxt.setText(loginEmail);
+        userPhoneTxt.setText(loginPhone);
+
 
     }
 
     @Override
     public void bindViews() {
-
+        this.userPhoneTxt = (TextView) findViewById(R.id.userPhoneTxt);
+        this.userEmailTxt = (TextView) findViewById(R.id.userEmailTxt);
+        this.userIDTxt = (TextView) findViewById(R.id.userIDTxt);
+        this.userNameTxt = (TextView) findViewById(R.id.userNameTxt);
     }
 }
